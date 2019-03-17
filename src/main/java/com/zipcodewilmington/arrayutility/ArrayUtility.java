9f6a0687d6b99cb95d6f072cf62a3c435ef0e664
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-03-17 19:39:48 -0400

    ugh

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -13,21 +13,26 @@ public class ArrayUtility<T> {
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T value) {
-           T[] genArr = mergeArrays(arrayToMerge);
-            return getNumberOfOccurrences(value);
+
+        genArr = mergeArrays(arrayToMerge);
+        return getNumberOfOccurrences(value);
     }
 
+
     public T[] mergeArrays(T[] arrayToMerge) {
+
         T[] answerArr = Arrays.copyOf(genArr, genArr.length + arrayToMerge.length);
 
         for (int i = 0; i < genArr.length; i++) {
             answerArr[i] = genArr[i];
+
         }
-        for (int j = 0; j < arrayToMerge.length; j++) {
-            answerArr[j] = arrayToMerge[j];
+        for (int i = genArr.length, j = 0; j < arrayToMerge.length; i++, j++) {
+            answerArr[i] = arrayToMerge[j];
         }
         return answerArr;
     }
+
 
     public Integer getNumberOfOccurrences(T object) {
         int counter = 0;
@@ -42,11 +47,14 @@ public class ArrayUtility<T> {
     }
 
     public T[] removeValue(T object) {
+
         Integer index = 0;
         T[] whatyaNeedtoReturn = Arrays.copyOf(genArr, genArr.length - getNumberOfOccurrences(object));
         for (int i = 0; i < genArr.length; i++) {
-            whatyaNeedtoReturn[index] = whatyaNeedtoReturn[i];
-            index++;
+            if (genArr[i] != object) {
+                whatyaNeedtoReturn[index] = genArr[i];
+                index++;
+            }
         }
         return whatyaNeedtoReturn;
     }
@@ -54,10 +62,10 @@ public class ArrayUtility<T> {
     public T getMostCommonFromMerge(T[] arrayToMerge) {
         Integer counter = 0;
         T mostCommonThing = null;
-        T[] answerArray = mergeArrays(arrayToMerge);
-        for (T thing : arrayToMerge) {
+        T[] aTArray = mergeArrays(arrayToMerge);
+        for (T thing : aTArray) {
             if (getNumberOfOccurrences(thing) > counter) {
-                counter = getNumberOfOccurrences(mostCommonThing);
+                counter = getNumberOfOccurrences(thing);
                 mostCommonThing = thing;
 
             }
@@ -65,5 +73,5 @@ public class ArrayUtility<T> {
         return mostCommonThing;
     }
 
-
 }
+
