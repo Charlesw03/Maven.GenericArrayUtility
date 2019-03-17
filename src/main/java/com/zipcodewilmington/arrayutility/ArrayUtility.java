a853a2bd97ab2b5d77d441fa0fd325642f7560bc
Charles Wilmer <charleswilmer@zipcoders-MacBook-Pro.local>
2019-03-17 16:17:06 -0400

    most of the logic is there but im only passing 5 of 16 :/

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,69 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] genArr;
+
+    public ArrayUtility(T[] genArr) {
+        this.genArr = genArr;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T value) {
+           T[] genArr = mergeArrays(arrayToMerge);
+            return getNumberOfOccurrences(value);
+    }
+
+    public T[] mergeArrays(T[] arrayToMerge) {
+        T[] answerArr = Arrays.copyOf(genArr, genArr.length + arrayToMerge.length);
+
+        for (int i = 0; i < genArr.length; i++) {
+            answerArr[i] = genArr[i];
+        }
+        for (int j = 0; j < arrayToMerge.length; j++) {
+            answerArr[j] = arrayToMerge[j];
+        }
+        return answerArr;
+    }
+
+    public Integer getNumberOfOccurrences(T object) {
+        int counter = 0;
+        for (T thing : genArr) {
+            if (thing == object) {
+                counter++;
+
+            }
+        }
+        return counter;
+
+    }
+
+    public T[] removeValue(T object) {
+        Integer index = 0;
+        T[] whatyaNeedtoReturn = Arrays.copyOf(genArr, genArr.length - getNumberOfOccurrences(object));
+        for (int i = 0; i < genArr.length; i++) {
+            whatyaNeedtoReturn[index] = whatyaNeedtoReturn[i];
+            index++;
+        }
+        return whatyaNeedtoReturn;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        Integer counter = 0;
+        T mostCommonThing = null;
+        T[] answerArray = mergeArrays(arrayToMerge);
+        for (T thing : arrayToMerge) {
+            if (getNumberOfOccurrences(thing) > counter) {
+                counter = getNumberOfOccurrences(mostCommonThing);
+                mostCommonThing = thing;
+
+            }
+        }
+        return mostCommonThing;
+    }
+
+
 }
